495d88343b3c9ba94e2778e4b14be74c79b50874
emil <emilbabazade29@gmail.com>
2020-12-10 14:11:46 +0200

    added sleepnight entity

--- app/src/main/java/emil/babazade/trackmysleep/database/SleepNight.kt
+++ app/src/main/java/emil/babazade/trackmysleep/database/SleepNight.kt
@@ -16,3 +16,19 @@
 
 package emil.babazade.trackmysleep.database
 
+import androidx.room.*
+
+@Entity(tableName = "daily_sleep_quality_table")
+data class SleepNight(
+    @PrimaryKey(autoGenerate = true)
+    var nightId: Long = 0L,
+
+    @ColumnInfo(name = "start_time_milli")
+    val startTimeMilli: Long = System.currentTimeMillis(),
+
+    @ColumnInfo(name = "end_time_milli")
+    val endTimeMilli: Long = startTimeMilli,
+
+    @ColumnInfo(name = "quality_rating")
+    val sleepQuality: Int = -1
+)
